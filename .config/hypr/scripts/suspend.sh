4a1c7ea8def402ef3c0a96ddd1aa6326fcab805e
Mads Kelberg <madskelberg@tutanota.com>
2023-11-20 17:24:13 +0100

    Fixed auto suspend

--- .config/hypr/scripts/suspend.sh
+++ .config/hypr/scripts/suspend.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-swayidle -w \
-	timeout 300 ' swaylock ' \
-	timeout 600 ' hyprctl dispatch dpms off' \
+swayidle -w timeout 300 'swaylock -f -c 000000' \
+	timeout 600 'hyprctl dispatch dpms off' \
+	resume 'hyprctl dispatch dpms on' \
 	timeout 900 'systemctl suspend' \
-	resume ' hyprctl dispatch dpms on' \
-	before-sleep 'swaylock'
+	before-sleep 'swaylock -f -c 000000' &
+# swayidle -w \
+# 	timeout 300 ' swaylock ' \
+# 	timeout 600 ' hyprctl dispatch dpms off' \
+# 	timeout 900 'systemctl suspend' \
+# 	resume ' hyprctl dispatch dpms on' \
+# 	before-sleep 'swaylock'
